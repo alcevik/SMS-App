6fead4119c7fada4ea4666cbd98af21167244193
alkim cevik <alkimcevik@gmail.com>
2020-09-05 23:45:04 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 account_sid = 'AC1fdcd17e1909a278b72b1f8bb99b45f9'
 auth_token = '76e0dfca2581702a6228c55aba77502f'
-my_cell = '+16038544140'
-my_twilio = '+12563048032'
+my_cell = '+15435434343'
+my_twilio = '+12563043434'
